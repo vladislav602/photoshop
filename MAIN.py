f44f89adffbb4f08d7027f0d9a992fb6fbe63ee8
sauceTartarys <orestmykhatsko@gmail.com>
2024-10-20 16:22:54 +0300

    ciwj

--- MAIN.py
+++ MAIN.py
@@ -1,5 +1,41 @@
+import os
 
 from PyQt5.QtWidgets import *
+from PIL import Image
+import os
+
+class PhotoManager:
+    def __init__(self):
+        self.photo = None
+        self.folder = None
+        self.filename = None
+
+
+    def loed(self):
+        imahe_path = os.path.join(self,folder, self,filename)
+        self.photo = Image.open(imahe_path)
+
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
+
+   def show_images(self):
+       pixels = pil2pixmap(self.photo)
+       pixels = pixels.scaledToWidth(500)
+       image_lbl.setPixmap(pixels)
+
+
 
 app = QApplication([])
 
@@ -36,7 +72,7 @@ line3.addWidget(sharpness)
 line3.addWidget(chb)
 
 
-app.setStyleSheet()
+app.setStyleSheet("""
          QWidget {
              background:  #OOff66;
          }
@@ -48,7 +84,21 @@ app.setStyleSheet()
         border-style: outset;
         font-family; Roboro;
         main-width: 6em:
+""")
 
+def open_folder():
+    photo_manager.folder = QFileDialog.getExistingDirectoryUrl()
+    files = os.listdir(photo_manager.folder)
+    list.clear()
+    list.addItems(files)
+
+
+def show_chjsen_image():
+    photo_manager.fiename = images_list.currentItem().text()
+    photo_manager.show_images(photo)
+
+images_list.connect(show_chosen_image)
+folder.clicked.connect(open_folder()
 window.setLayout(mainline)
 window.show()
 app.exec()
