06cf8e01f416f1efd93b8a3641fee0c3149c0f66
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2024-10-27 16:24:14 +0200

    vhvhv

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,9 @@
 import os
 
+import ss
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
-from PIL import Image
+from PIL import Image, ImageFilter
 import os
 
 class PhotoManager:
@@ -9,11 +11,45 @@ class PhotoManager:
         self.photo = None
         self.folder = None
         self.filename = None
-
+        self.image_lbl= None
 
     def loed(self):
-        imahe_path = os.path.join(self,folder, self,filename)
+        imahe_path = os.path.join(self,folder, self.filename)
         self.photo = Image.open(imahe_path)
+
+
+    def bw(self):
+        self.photo = self.photo("L")
+        self.show_image(self.image_lbl)
+
+
+    def leften(self):
+        self.photo = self.photo.transpose(Image.ROTATE_90)
+        self.show_image(self.image_lbl)
+
+    def mirron (self):
+        self.photo = self.photo.transpose(Image.FLIP_LEFT_RIGHT)
+        self.show_image(self.image_lbl)
+
+    def risk(self):
+        self.photo = self.photo.filter(ImageFilter.SHARPEN)
+        self.show_image(self.image_lbl)
+
+
+    def rr(self):
+       self.photo = self.photo.filter(ImageFilter.CONTOUR)
+       self.show_image(self.image_lbl)
+
+
+    def ss(self):
+       self.photo = self.photo.filter(ImageFilter.DETAIL)
+
+       def aa(self):
+           self.photo = self.photo.filter(ImageFilter.BLUR)
+
+       def hh(self):
+           self.photo = self.photo.filterImageEnhance.Color(self.photo).enhance(1.5)
+
 
 def pil2pixmap(im):
     if im.mode == "RGB":
@@ -30,10 +66,10 @@ def pil2pixmap(im):
     pixmap = QPixmap.fromImage(qim)
     return pixmap
 
-   def show_images(self):
-       pixels = pil2pixmap(self.photo)
-       pixels = pixels.scaledToWidth(500)
-       image_lbl.setPixmap(pixels)
+def show_images(self):
+   pixels = pil2pixmap(self.photo)
+   pixels = pixels.scaledToWidth(500)
+   image_lbl.setPixmap(pixels)
 
 
 
@@ -41,13 +77,19 @@ app = QApplication([])
 
 window = QWidget()
 folder = QPushButton("Папка")
+image_lbl = QLabel("Картинка")
 list = QListWidget()
 photo = QLabel("фото")
 left = QPushButton("Вліво")
-right = QPushButton("Вправо")
+rr = QPushButton("Накладання конрурів")
 mirror = QPushButton("Дзеркало")
 sharpness = QPushButton("Різкість")
 chb = QPushButton("Ч/Б")
+
+
+hh = QPushButton("Збільшення насиченості")
+aa = QPushButton("Розмивання")
+sss =  QPushButton("Збшльшення детфлізації")
 
 
 
@@ -66,7 +108,7 @@ line2.addWidget(photo)
 line2.addLayout(line3)
 
 line3.addWidget(left)
-line3.addWidget(right)
+line3.addWidget(rr)
 line3.addWidget(mirror)
 line3.addWidget(sharpness)
 line3.addWidget(chb)
@@ -86,6 +128,8 @@ app.setStyleSheet("""
         main-width: 6em:
 """)
 
+photo_manager = PhotoManager()
+photo_manager.image_lbl = image_lbl
 def open_folder():
     photo_manager.folder = QFileDialog.getExistingDirectoryUrl()
     files = os.listdir(photo_manager.folder)
@@ -93,12 +137,20 @@ def open_folder():
     list.addItems(files)
 
 
-def show_chjsen_image():
-    photo_manager.fiename = images_list.currentItem().text()
+def show_chesen_image():
+    photo_manager.fiename = list.currentItem().text()
     photo_manager.show_images(photo)
 
-images_list.connect(show_chosen_image)
-folder.clicked.connect(open_folder()
+list.currentRowChanged.connect(show_chesen_image)
+chb.clicked.connect(photo_manager.bw)
+left.clicked.connect(photo_manager.leften)
+mirror.clicked.connect(photo_manager.mirron)
+rr.clicked.connect(photo_manager.rr)
+sss.clicked.connect(photo_manager.ss)
+aa.clicked.connect(photo_manager.aa)
+hh.clicked.connect(photo_manager.hh)
+sharpness.clicked.connect(photo_manager.risk())
+folder.clicked.connect(open_folder)
 window.setLayout(mainline)
 window.show()
 app.exec()
