7836c28b26ca0d7c87470fc0616cf45d2910141f
Anonymous <orestmykhatsko@gmail.com>
2024-11-10 15:17:02 +0200

    vlad

--- MAIN.py
+++ MAIN.py
@@ -1,6 +1,5 @@
 import os
 
-import ss
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
 from PIL import Image, ImageFilter
@@ -13,42 +12,48 @@ class PhotoManager:
         self.filename = None
         self.image_lbl= None
 
-    def loed(self):
-        imahe_path = os.path.join(self,folder, self.filename)
-        self.photo = Image.open(imahe_path)
+    def load(self):
+        image_path = os.path.join(self.folder, self.filename)
+        self.photo = Image.open(image_path)
+
+    def show_images(self, image_lbl):
+
+        pixels = pil2pixmap(self.photo)
+        pixels = pixels.scaledToWidth(500)
+        self.image_lbl.setPixmap(pixels)
 
 
     def bw(self):
-        self.photo = self.photo("L")
-        self.show_image(self.image_lbl)
+        self.photo = self.photo.convert("L")
+        self.show_images(self.image_lbl)
 
 
     def leften(self):
         self.photo = self.photo.transpose(Image.ROTATE_90)
-        self.show_image(self.image_lbl)
+        self.show_images(self.image_lbl)
 
     def mirron (self):
         self.photo = self.photo.transpose(Image.FLIP_LEFT_RIGHT)
-        self.show_image(self.image_lbl)
+        self.show_images(self.image_lbl)
 
     def risk(self):
         self.photo = self.photo.filter(ImageFilter.SHARPEN)
-        self.show_image(self.image_lbl)
+        self.show_images(self.image_lbl)
 
 
     def rr(self):
        self.photo = self.photo.filter(ImageFilter.CONTOUR)
-       self.show_image(self.image_lbl)
+       self.show_images(self.image_lbl)
 
 
     def ss(self):
        self.photo = self.photo.filter(ImageFilter.DETAIL)
 
-       def aa(self):
-           self.photo = self.photo.filter(ImageFilter.BLUR)
+    def aa(self):
+       self.photo = self.photo.filter(ImageFilter.BLUR)
 
-       def hh(self):
-           self.photo = self.photo.filterImageEnhance.Color(self.photo).enhance(1.5)
+    def hh(self):
+       self.photo = self.photo.filterImageEnhance.Color(self.photo).enhance(1.5)
 
 
 def pil2pixmap(im):
@@ -74,7 +79,20 @@ def show_images(self):
 
 
 app = QApplication([])
+app.setStyleSheet("""
+         QWidget {
+             background-color:  #00ff66;
+         }
 
+
+        QPushButton
+        {
+        background-color: #66ff00;
+        border-style: outset;
+        font-family; Roboto;
+        main-width: 6em:
+        }
+""")
 window = QWidget()
 folder = QPushButton("Папка")
 image_lbl = QLabel("Картинка")
@@ -114,31 +132,20 @@ line3.addWidget(sharpness)
 line3.addWidget(chb)
 
 
-app.setStyleSheet("""
-         QWidget {
-             background:  #OOff66;
-         }
 
-
-        QPushButton
-        {
-        backgroud-color: #66ff00;
-        border-style: outset;
-        font-family; Roboro;
-        main-width: 6em:
-""")
 
 photo_manager = PhotoManager()
-photo_manager.image_lbl = image_lbl
+photo_manager.image_lbl = photo
 def open_folder():
-    photo_manager.folder = QFileDialog.getExistingDirectoryUrl()
+    photo_manager.folder = QFileDialog.getExistingDirectory()
     files = os.listdir(photo_manager.folder)
     list.clear()
     list.addItems(files)
 
 
 def show_chesen_image():
-    photo_manager.fiename = list.currentItem().text()
+    photo_manager.filename = list.currentItem().text()
+    photo_manager.load()
     photo_manager.show_images(photo)
 
 list.currentRowChanged.connect(show_chesen_image)
@@ -149,7 +156,7 @@ rr.clicked.connect(photo_manager.rr)
 sss.clicked.connect(photo_manager.ss)
 aa.clicked.connect(photo_manager.aa)
 hh.clicked.connect(photo_manager.hh)
-sharpness.clicked.connect(photo_manager.risk())
+sharpness.clicked.connect(photo_manager.risk)
 folder.clicked.connect(open_folder)
 window.setLayout(mainline)
 window.show()
